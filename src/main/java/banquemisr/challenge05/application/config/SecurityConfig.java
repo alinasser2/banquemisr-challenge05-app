51ca8ad6ab7960d6ca4506b99341d353b1fcbb52
alinasser2 <alinasserelden@hotmail.com>
2024-12-04 22:16:16 +0200

    move config file to a more proper directory

--- src/main/java/banquemisr/challenge05/application/config/SecurityConfig.java
+++ src/main/java/banquemisr/challenge05/application/config/SecurityConfig.java
@@ -1,6 +1,7 @@
-package banquemisr.challenge05.application.security;
+package banquemisr.challenge05.application.config;
 
 
+import banquemisr.challenge05.application.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
